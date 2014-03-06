b0bdb1c4fa1ed859ba981d17d9b832bdb9535117
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2014-03-05 21:10:01 -0300

    Teste 3 elementos

--- main.cpp
+++ main.cpp
@@ -1,35 +1,40 @@
 #include <stdio.h>
 #include "runner.h"
 
-int soma(int a, int b){
-	return a+b;
-}
+#define N 100
 
-int teste_soma_positivo(){
-	_assert( soma(1,1) == 2 );
-	_assert( soma(2,2) != 5 );
-	
+int getIndiceEquilibrio(int* v, int n){
+	if(n == 3){
+		if(v[0] == v[2]){
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
 	return 0;
 }
 
-int teste_soma_negativo(){
-	_assert( soma(-1, -1) == -2 );
-	_assert( soma(-2, -2) != -4 );
-	
+int teste_basico(){
+	int vetor1[N] = {0};
+	_assert( getIndiceEquilibrio(vetor1, 1) == 0 );
+
 	return 0;
 }
 
-int teste_soma_alternado(){
-	_assert( soma(1, -1) == 0 );
-	_assert( soma(1, -2) == -1 );
-	
+int teste_tres_elementos(){
+	int vetor1[N] = {1, 2, 1};
+	_assert( getIndiceEquilibrio(vetor1, 3) == 1);
+
+	int vetor2[N] = {1, 2, 2};
+	_assert( getIndiceEquilibrio(vetor2, 3) == -1);
+
 	return 0;
 }
 
 int todos_testes() {
-	_verify(teste_soma_positivo);
-	_verify(teste_soma_negativo);
-	_verify(teste_soma_alternado);
+	_verify(teste_basico);
+	_verify(teste_tres_elementos);
 	
 	return 0;
 }
